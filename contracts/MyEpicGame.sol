c02b613578ee0dccc4235e5368dafda752253220
Nestor Mamani <nmamanipantoja@gmail.com>
2021-10-24 10:26:58 -0500

    Minted NFT and watch data in JSON

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -9,6 +9,8 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 import 'hardhat/console.sol';
 
+import './libraries/Base64.sol';
+
 contract MyEpicGame is ERC721{
   struct CharacterAttributes {
     uint characterIndex;
@@ -72,5 +74,36 @@ contract MyEpicGame is ERC721{
     nftHolders[msg.sender] = newItemId;
 
     _tokenIds.increment();
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory){
+    CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+    string memory strHp = Strings.toString(charAttributes.hp);
+    string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+    string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"name": "',
+            charAttributes.name,
+            ' -- NFT #: ',
+            Strings.toString(_tokenId),
+            '", "description": "This is an NFT that lets people play in the game Road to Trillion!", "image": "',
+            charAttributes.imageURI,
+            '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+            strAttackDamage,'} ]}'
+          )
+        )
+      )
+    );
+
+    string memory output = string(
+      abi.encodePacked("data:application/json;base64,", json)
+    );
+    
+    return output;
   }
 }
